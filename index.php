3b7a295181c2ee37d5aaf541dddc4f8fa5637841
Giedrius Rimkus <info@giedriusr.lt>
2013-01-30 17:05:38 +0200

    initial commit of dirtrally.de

---
+++ index.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <title>Dirt Rally - rally manager online</title>
+    <meta http-equiv="content-Type" content="text/html; charset=utf-8" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    <link href="https://plus.google.com/111569305973619963063" rel="publisher" />
+    <meta name="author" content="Giedrius Rimkus" />
+    <meta name="description" content="Dirt rally is online rally management game! You will manage your own rally team!" />
+    <meta name="keywords" content="rally manager, dirt rally, online rally manager, rally manager online, web-based manager, rally management game, world rally manager, rally game, rally, rally cars, rally crew, online manager, dirtrally, ralis, ralio zaidimas lietuvoje, ralis lietuva" />
+    <meta name="y_key" content="5508d08f36b3a694" /><!-- yahoo -->
+    <link rel="stylesheet" href="common.css" type="text/css" />
+    <link rel="stylesheet" href="require.css" type="text/css" />
+    <link rel="stylesheet" href="style.css" type="text/css" />
+
+    <meta property="og:title" content="Dirt Rally - online rally manager!" />
+    <meta property="og:type" content="company" />
+    <meta property="og:site_name" content="Dirt Rally" />
+    <meta property="og:url" content="http://www.dirtrally.net" />
+    <meta property="og:image" content="null" />
+    <meta property="og:description" content="I've just signed up for being informed when game goes live!" />
+  </head>
+
+  <body>
+
+    <p align="center"><img src="dirt-trac-logo.png" alt="Dirt Rally" /></p>
+    <h1>Dirt Rally - online rally manager</h1>
+    <br />
+    <p align="center" class="middle justify">
+<strong>What it's all about?</strong><br />
+
+The main idea of the game - to manage virtual rally team and win the championship in the highest group “World Class”.
+All registered users become virtual managers, who has the rights to manage rally teams, meaning that you have to deal with: cars, finances, hiring drivers / co-drivers.<br><br>
+The game has teams by default, these teams are managed automatically by “robots / not real users”. These teams have amateur drivers and co-drivers. Once signed up, new managers are be able to give a new name to their team. Also, all teams have 2 different cars, drivers, co-drivers, mechanics. Cars have 1st level parts. Car types: 4WD and 2WD. Parts could be upgraded up to 10th level.<br><br>
+How to survive during the season? Money are being transfered to all teams after every rally event dependent on their rally results. Money from sponsors are also available. Teams have to pay salaries to it’s crew: drivers, co-drivers, mechanics. Cars can be damaged a lot during the rally events, so you have to give orders which parts must be repaired by your team's mechanics. In order to get better results, managers have to train their crew every week.<br><br>
+Every season lasts 15 weeks, meaning 15 different rally events. One full rally even lasts for 3 days as well as it is in the real world rally championship. On the 16th week, there is a race called “Race of Champions”, where winners of each group compete for the world title.<br><br>
+Interested? We can let you know, when we are ready!
+<br>
+<a href="http://www.facebook.com/media/set/?set=a.10150952283343947.409947.172787453946&type=3">Development progress so far..</a>
+<div rel="CFAL1PRL" class="lrdiscoverwidget" data-logo="on" data-background="off" data-share-url="www.dirtrally.net" data-css=""></div><script type="text/javascript" src="http://launchrock-ignition.s3.amazonaws.com/ignition.1.1.js"></script>
+<br />
+    </p>
+<br />
+
+    <div style="text-align: center; margin: 10px auto; width: 400px">
+   <a href="https://twitter.com/dirtrally" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @dirtrally</a>
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+<br><br>
+  Read more about our <a href="https://github.com/giedriusr/dirtrally-logo-competition">logo competition here</a>!<br>
+  Also, <a href="http://blog.giedriusr.lt/tag/dirtrallydevelopment">Dirt Rally Development Blog</a>!<br>
+      <?php include('includes/keywords.html') ?>
+    </div>
+
+
+    <?php include('includes/facebook-fanbox.html') ?>
+    <?php include('includes/follow-us.php') ?>
+    <?php include('includes/google-analytics.html') ?>
+
+      <div style="text-align: center; margin-top: 10px;">
+      <?php include("includes/heylt.html"); ?>
+    </div>
+<script type='text/javascript'>
+
+var _ues = {
+host:'dirtrally.userecho.com',
+forum:'13622',
+lang:'en',
+tab_icon_show:false,
+tab_corner_radius:5,
+tab_font_size:20,
+tab_image_hash:'RmVlZGJhY2s%3D',
+tab_alignment:'right',
+tab_text_color:'#FFFFFF',
+tab_bg_color:'#FFCB96',
+tab_hover_color:'#F44C4C'
+};
+
+(function() {
+    var _ue = document.createElement('script'); _ue.type = 'text/javascript'; _ue.async = true;
+    _ue.src = ('https:' == document.location.protocol ? 'https://s3.amazonaws.com/' : 'http://') + 'cdn.userecho.com/js/widget-1.4.gz.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(_ue, s);
+  })();
+
+</script>
+<p align="center">
+  <a class="addthis_button_google_plusone_badge" g:plusone:size="badge" g:plusone:href="https://plus.google.com/111569305973619963063"></a>
+  <!-- AddThis Button BEGIN -->
+  <script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+  <script type="text/javascript" src="http://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5093eb49401bda43"></script>
+  <!-- AddThis Button END -->
+</p>
+
+  </body>
+</html>
